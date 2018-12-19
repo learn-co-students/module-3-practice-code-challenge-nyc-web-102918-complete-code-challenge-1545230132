485350a26642ebbab18a2ab923cf269fffa8d6d7
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2018-12-19 10:58:20 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 1654 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,68 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  const uLComments = document.querySelector('#comments')
+
+  const likeButton =
+  document.querySelector('#like_button')
+
+  const likes =
+  document.querySelector('#likes')
+
+  const commnets = document.querySelector('#comment_input')
+
+  const commentForm =
+  document.querySelector('#comment_form')
+
+fetch(`https://randopic.herokuapp.com/images/${imageId}`)
+.then(r=>r.json())
+.then(r=>{
+  uLComments.innerHTML = `<li>${r.comments[0].content}</li>`
 })
+
+likeButton.addEventListener('click', (e)=>{
+  let currentLikes = parseInt(likes.innerText)
+  currentLikes += 1
+  likes.innerText = currentLikes
+
+
+
+  fetch('https://randopic.herokuapp.com/likes', {
+        method: 'Post',
+        headers: {'Content-type': 'application/json', Accept: 'application/json'},
+        body: JSON.stringify({
+        image_id: 1654
+        // like_count: currentLikes
+          })
+
+        })
+        .then(r => r.json())
+})
+
+commentForm.addEventListener('submit', (e)=>{
+  e.preventDefault()
+
+  let newComment = document.querySelector('#comment_input').value
+  uLComments.innerHTML += `<li>${newComment}</li>
+`
+
+  fetch('https://randopic.herokuapp.com/comments', {
+        method: 'Post',
+        headers: {'Content-type': 'application/json', Accept: 'application/json'},
+        body: JSON.stringify({
+        image_id: 1654,
+        content: newComment
+          })
+
+        })
+        .then(r => r.json())
+})
+
+
+
+
+
+
+
+
+})//end of dom content loaded
